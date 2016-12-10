8d76dd61d29072b9d56b0d1f7825ff58e216ae9a
Lise Gorelick <egoreli@umich.edu>
2016-12-09 19:41:28 -0500

    deleted home.js

--- app/controllers/home.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('myApp.controllers.home', [])
-
-// Home controller
-.controller('HomeCtrl', function($scope, $rootScope, firebaseData) {
-     $scope.isCollapsed = true;
-});
