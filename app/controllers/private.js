ab66af31023070321f9d07c634ded423eb79380a
sartechb <sarthak1657@gmail.com>
2016-12-09 20:23:42 -0500

    firepad changes

--- app/controllers/private.js
+++ app/controllers/private.js
@@ -4,5 +4,14 @@ angular.module('myApp.controllers.private', [])
 
 // Homepage controller
 .controller('PrivateCtrl', function($scope, $rootScope, firebaseData) {
-     $scope.isCollapsed = true;
+    $scope.isCollapsed = true;
+    var firepadRef = firebaseData.database().ref();
+    var codeMirror = CodeMirror(document.getElementById('document-wrapper'), { lineWrapping: true });
+    var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
+        richTextShortcuts: true,
+        richTextToolbar: true,
+        defaultText: 'Hello, World!'
+    });
+
+
 });
