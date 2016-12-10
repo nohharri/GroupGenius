232f9f84a8ef1b57a1b2f90f699e7ff60b39eb80
sartechb <sarthak1657@gmail.com>
2016-12-09 19:01:47 -0500

    updated firebase references

--- app/lib/services.js
+++ app/lib/services.js
@@ -3,11 +3,25 @@
 angular.module('myApp.lib.services', [])
 
 .factory('firebaseData', function($q) {
-    var FB_NAME = "https://YOUR-FIREBASE-REF.firebaseio.com/";
+    // var FB_NAME = "https://YOUR-FIREBASE-REF.firebaseio.com/";
+    var config = {
+        apiKey: "AIzaSyDZJjvkGq7oy1gP753Rgi84PL5GoAF2egg",
+        authDomain: "groupgenius-5953b.firebaseapp.com",
+        databaseURL: "https://groupgenius-5953b.firebaseio.com",
+        storageBucket: "groupgenius-5953b.appspot.com",
+        messagingSenderId: "849759934347"
+    };
+
+    firebase.initializeApp(config);
+
     return {
-        ref: function () {
-            return new Firebase(FB_NAME);
+        database: function() {
+            return firebase.database();
         },
+        storage: function () {
+            return firebase.storage();
+        },
+
     }
 })
 
