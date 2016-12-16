b277bf8fa3dd9c4d53eab377535433b988e5ad98
sartechb <sarthak1657@gmail.com>
2016-12-16 17:21:58 -0500

    start of new chat system

--- app/controllers/private.js
+++ app/controllers/private.js
@@ -5,6 +5,8 @@ angular.module('myApp.controllers.private', [])
 // Homepage controller
 .controller('PrivateCtrl', function($scope, $rootScope, firebaseData) {
     $scope.isCollapsed = true;
+    $scope.messages = [];
+
     var firepadRef = firebaseData.database().ref();
     var codeMirror = CodeMirror(document.getElementById('wrapper-document'), { lineWrapping: true });
     var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
@@ -14,20 +16,29 @@ angular.module('myApp.controllers.private', [])
     });
 
 
-    firebaseData.provider().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var chatRef = firebaseData.database().ref("chat");
-        // Create a Firechat instance
-        var chat = new FirechatUI(chatRef, document.getElementById("wrapper-chat"));
-        chat.setUser(user.uid, getUniqname(user.email));
+    // firebaseData.provider().onAuthStateChanged(function(user) {
 
-    } else {
-        // No user is signed in.
-        console.log("need auth");
-        console.log(user);
-    }
-});
+        var chatRef = firebaseData.database().ref('chat');
+        chatRef.off();
+        var setMessage = function(data){
+            console.log("set message called");
+            console.log(data.val());
+            
+            console.log($scope.messages);
+            $scope.$apply(function() {
+                $scope.messages.push(data.val());
+            });
+        }
+
+        chatRef.limitToLast(12).on('child_added', setMessage);
+        chatRef.limitToLast(12).on('child_changed', setMessage);
+
+
+        // var chatRef = firebaseData.database().ref("chat");
+        // // Create a Firechat instance
+        // var chat = new FirechatUI(chatRef, document.getElementById("wrapper-chat"));
+        // chat.setUser(user.uid, getUniqname(user.email));
+    // });
 
 
 
