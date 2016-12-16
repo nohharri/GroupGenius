cea2165a15157a4fc32c119a6ad016884c73e82c
sartechb <sarthak1657@gmail.com>
2016-12-15 20:36:25 -0500

    firechat changes

--- app/controllers/private.js
+++ app/controllers/private.js
@@ -14,4 +14,27 @@ angular.module('myApp.controllers.private', [])
     });
 
 
+    firebaseData.provider().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var chatRef = firebaseData.database().ref("chat");
+        // Create a Firechat instance
+        var chat = new FirechatUI(chatRef, document.getElementById("wrapper-chat"));
+        chat.setUser(user.uid, getUniqname(user.email));
+
+    } else {
+        // No user is signed in.
+        console.log("need auth");
+        console.log(user);
+    }
 });
+
+
+
+});
+
+
+function getUniqname(email){
+    console.log(email);
+    return email.substring(0,email.indexOf('@'));
+}
