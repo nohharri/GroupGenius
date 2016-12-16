68e3122f555db110f7b0fb1d6a63617f107a6f44
stali <charscape9001@gmail.com>
2016-12-17 07:28:00 +1100

    working on public

--- app/controllers/public.js
+++ app/controllers/public.js
@@ -5,12 +5,7 @@ angular.module('myApp.controllers.public', [])
 .controller('PublicCtrl', function($scope, $rootScope, firebaseData, headerService) {
      $scope.isCollapsed = true;
      $scope.allGroups = groups;
-     $scope.updateGroup = function(name, desc, members, spots) {
-     	$scope.currentGroup = name;
-     	$scope.currentMembers = 'Current Members: ' + formatMembers(members);
-     	$scope.openSpots = 'Open Spots: ' + spots;
-     	$scope.desc = 'Description: ' + desc;
-     };
+	$scope.currentGroup = "Select a Group!"
      $scope.formatMembers = function(members) {
 		var s = ' ';
 		for( var i = 0; i < members.length; ++i) {
@@ -22,12 +17,18 @@ angular.module('myApp.controllers.public', [])
 
 		return s;
 	};
+     $scope.updateGroup = function(name, desc, members, spots) {
+     	$scope.currentGroup = name;
+     	$scope.currentMembers = 'Current Members: ' + $scope.formatMembers(members);
+     	$scope.openSpots = 'Open Spots: ' + spots;
+     	$scope.desc = 'Description: ' + desc;
+     };
     //åheaderService.set("different");
 });
 
 
 var groups = [
-	{ "name": "Group #1", "desc": "Desc", "members": ["Dina Rudelston", "Megan Helena"], "spots": 4},
+	{ "name": "Group #1", "desc": "orem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ante velit, faucibus vitae lacus ut, pellentesque venenatis lorem. Cras condimentum sodales posuere. In accumsan lorem vel posuere mollis. Sed non convallis sem. In ornare eros et fringilla fringilla. Proin tincidunt ipsum vel dui congue, in fringilla tortor cursus. Nullam feugiat lectus nec dui consectetur, in elementum massa finibus. Etiam accumsan nisl velit, a auctor sapien consectetur eget. Integer eu neque ante. Aliquam at justo non elit porttitor mattis vel ac velit.", "members": ["Dina Rudelston", "Megan Helena"], "spots": 4},
 	{ "name": "Group #2", "desc": "Desc2", "members": ["Lise Gorelick", "Mav the Maverick"], "spots": 3}
 ];
 
