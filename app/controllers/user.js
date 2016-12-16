9cf8c06612f809975a2c9a6a6fa40f160f9835e7
mavcook <mavcook@umich.edu>
2016-12-16 18:32:18 -0500

    Side bar completed (jquery anchor scroll)

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -4,5 +4,12 @@ angular.module('myApp.controllers.user', [])
 
 // Homepage controller
 .controller('UserCtrl', function($scope, $rootScope, firebaseData) {
-     $scope.isCollapsed = true;
+	 $scope.isCollapsed = true;
+	 $scope.scrollTo = function (target){
+		var container = $('#wrapper-main'), scrollTo = $(target);
+		container.animate({
+			scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop() 
+		});
+	}
 });
+
