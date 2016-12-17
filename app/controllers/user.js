b17400f537fb3fe47471c5d3b24090daad3a9ff1
mavcook <mavcook@umich.edu>
2016-12-16 20:09:22 -0500

    user page template mostly done

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,13 +3,28 @@
 angular.module('myApp.controllers.user', [])
 
 // Homepage controller
-.controller('UserCtrl', function($scope, $rootScope, firebaseData) {
+.controller('UserCtrl', function($scope, $rootScope, firebaseData, $http) {
 	 $scope.isCollapsed = true;
-	 $scope.scrollTo = function (target){
-		var container = $('#wrapper-main'), scrollTo = $(target);
+
+	 // Fix for not being able to use anchors
+	 $scope.scrollTo = function (target)
+	 {
+		var container = $('#wrapper-main'), scrollTo = $('#' + target);
 		container.animate({
 			scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop() 
 		});
 	}
+
+
+	// Grabs data from firebase like username and the groups they are in
+	$scope.getData = function()
+	{
+		var data = [];
+		$http.get('https://groupgenius-5953b.firebaseio.com/groups.json').success(function (response) {
+			console.log(response);
+			$scope.userData = response;
+		});
+	};
+
 });
 
