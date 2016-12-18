66dc662aa873edef8752b196ce16c2700459dd0d
Harrison Noh <hyuntaek@yahoo.com>
2016-12-17 19:52:49 -0500

    rerouted private create group to private

--- app/controllers/public.js
+++ app/controllers/public.js
@@ -15,6 +15,8 @@ angular.module('myApp.controllers.public', [])
 	$scope.nametoemail = {};
 	$scope.currEmails = [];
 	$scope.unlChecked = true;
+	$scope.groupId;
+
 	$scope.createUserHashMap = function(){
 		$http({
 			method: 'GET',
@@ -59,42 +61,46 @@ angular.module('myApp.controllers.public', [])
 	$scope.getClasses();
 	$scope.currentClass = $scope.classes[0];
 	$scope.currentGroup = "Select a Group!"
-	$scope.isActiveClass = function(clas){
-		if($scope.currentClass == clas){
-			return true;
-		} return false;
-		}
 
-     $scope.updateGroup = function(name, desc, members, spots) {
-	$scope.selected = true;
-     	$scope.currentGroup = name;
-     	$scope.currentMembers = $scope.formatMembers(members);
-	if(spots == -1){
-		$scope.openSpots = 'Open Spots: Unlimited';
-	} else {
-     		$scope.openSpots = 'Open Spots: ' + spots;
+	$scope.isActiveClass = function(clas){
+		if($scope.currentClass == clas) {
+			return true;
+		}
+		return false;
 	}
-     	$scope.desc = desc;
-     };
+
+	$scope.updateGroup = function(groupId, name, desc, members, spots) {
+		$scope.selected = true;
+		$scope.currentGroup = name;
+		$scope.currentMembers = $scope.formatMembers(members);
+
+		if(spots == -1) {
+			$scope.openSpots = 'Open Spots: Unlimited';
+		} else {
+	 		$scope.openSpots = 'Open Spots: ' + spots;
+		}
+		$scope.desc = desc;
+
+		//console.log(group);
+		$scope.groupId = groupId;
+	}
+	
 	$scope.selCls = function(clas) {
 		$scope.currentClass = clas;
 	}
-     $scope.formatMembers = function(members) {
+	
+	$scope.formatMembers = function(members) {
 		var s = ' ';
 		for( var i = 0; i < members.length; ++i) {
-			
 			s += members[i];
-			if( i < members.length - 1)
+			if( i < members.length - 1) {
 				s += ", ";
+			}
 		}
-
 		return s;
-	};
-
-
+	}
 
 	$scope.createNewGroup = function() {
-
 		console.log("create new group");
 		if(!$scope.newName || !$scope.newDesc) {
 			alert("You must designate a name and description. Your group has NOT been created!");
@@ -116,51 +122,55 @@ angular.module('myApp.controllers.public', [])
 
 	$scope.writeNewPost = function(name, desc, spots, org, mustApprove) {
 
-	console.log("writing a new post");
+		console.log("writing a new post");
 
-	var members = [];
-	//add the creator of the group to the members
-	members.push(firebase.auth().currentUser.uid);
-	
-	// Generate group id (new groupId = number of groups + 1)
-	var groupId = 0;
-	$.ajax({
-		dataType: 'JSON',
-		url: 'https://groupgenius-5953b.firebaseio.com/groups.json',
-		success: function(resp) {groupId = Object.keys(resp).length + 1},
-		async: false // need to wait for response
-	});
-  // A post entry.
-  var newGroup = {
-    name: name,
-    desc: desc,
-    members: members,
-    spots: spots,
-    mustApprove: mustApprove,
-    org: org,
-	groupId: 0 // null at first
-  };
+		var members = [];
+		//add the creator of the group to the members
+		members.push(firebase.auth().currentUser.uid);
+		
+		// Generate group id (new groupId = number of groups + 1)
+		var groupId = 0;
+		$.ajax({
+			dataType: 'JSON',
+			url: 'https://groupgenius-5953b.firebaseio.com/groups.json',
+			success: function(resp) {groupId = Object.keys(resp).length + 1},
+			async: false // need to wait for response
+		});
+	  // A post entry.
+	  var newGroup = {
+	    name: name,
+	    desc: desc,
+	    members: members,
+	    spots: spots,
+	    mustApprove: mustApprove,
+	    org: org,
+		groupId: 0 // null at first
+	  };
 
-  // Get a key for a new Post.
-  var newPostKey = firebase.database().ref().child('groups').push().key;
+	  // Get a key for a new Post.
+	  var newPostKey = firebase.database().ref().child('groups').push().key;
 
-  // Create default chat
-  var chatRef = firebase.database().ref('/chat/' + newPostKey + '/' + 'General');
-  var keyName = chatRef.push({
-  	name: 'Chatbot',
-  	text: 'Welcome to the stream!'
-  });
+	  // Create default chat
+	  var chatRef = firebase.database().ref('/chat/' + newPostKey + '/' + 'General');
+	  var keyName = chatRef.push({
+	  	name: 'Chatbot',
+	  	text: 'Welcome to the stream!'
+	  });
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  newGroup['groupId'] = newPostKey; // update groupId
-  //write the group to the group list
-  updates['/groups/' + newPostKey] = newGroup;
- 
-  firebase.database().ref().update(updates);
+	  // Write the new post's data simultaneously in the posts list and the user's post list.
+	  var updates = {};
+	  newGroup['groupId'] = newPostKey; // update groupId
+	  //write the group to the group list
+	  updates['/groups/' + newPostKey] = newGroup;
+	 
+	  firebase.database().ref().update(updates);
 
-  //window.location.href = '/app/#/private?groupId=' + newPostKey;
-}
+	  //window.location.href = '/app/#/private?groupId=' + newPostKey;
+	}
+
+	$scope.joinGroup = function() {
+		window.location.href = '/app/#/private?groupId=' + $scope.groupId;	
+	}
     
 });
 
